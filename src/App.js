5aaf4e19f13da8f5dd6c337a4fbf9daf5cb82874
AndrewJetLee <andrewjetlee777@gmail.com>
2022-02-12 16:39:36 -0600

    Created Home page component and Nav component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Router path="/">
+        <Home />
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Nav from "../components/Nav";
+
+const Home = () => {
+  return (
+    <div>
+        <Nav/>
+        Home
+    </div>
+  )
+}
+
+export default Home
