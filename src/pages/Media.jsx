f2768da0db6f42435dfa7400d19a8e7f05ef7f16
AndrewJetLee <andrewjetlee777@gmail.com>
2022-02-16 20:53:33 -0600

    Added styled components to Media component

--- src/pages/Media.jsx
+++ src/pages/Media.jsx
@@ -5,12 +5,44 @@ const Media = () => {
   const location = useLocation();
   console.log(location.state);
   return (
-    <Container>Media</Container>
+    <Container>
+        <Left>
+            <Image></Image>
+            <Information></Information>
+            <Statistics></Statistics>
+        </Left>
+        <Right>
+            <Details></Details>
+            <Synopsis></Synopsis>
+        </Right>
+    </Container>
   )
 }
 
 export default Media
 
 const Container = styled.div`
+    
+`
+
+//Left Column
+const Left = styled.div`
+    
+`
+const Image = styled.div`
+    
+`
+const Information = styled.div`
+`
+const Statistics = styled.div``
+
+// Right Column
+const Right = styled.div`
+    
+`
+const Details = styled.div`
+    
+`
+const Synopsis = styled.div`
     
 `
