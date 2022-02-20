dae1f95589284fbd2d56f88d8905a25a9ae53979
AndrewJetLee <andrewjetlee777@gmail.com>
2022-02-19 18:56:29 -0600

    Created validatePassword helper function

--- server/utility/passwordHelpers.js
+++ server/utility/passwordHelpers.js
@@ -1,7 +1,7 @@
 const crypto = require("crypto");
 
 
-module.exports.encryptPassword = async (password) => {
+module.exports.encryptPassword = (password) => {
     let salt = crypto.randomBytes(32).toString("hex");
     let hash = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString("hex");
     return {
@@ -10,3 +10,7 @@ module.exports.encryptPassword = async (password) => {
     }
 }   
 
+module.exports.validatePassword = (password, hash, salt) => {
+    let inputtedHash = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString("hex");
+    return hash === inputtedHash; 
+}   
