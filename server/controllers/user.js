f015469dd1841bfaf2adc1854e4db76b9db7ad88
AndrewJetLee <andrewjetlee777@gmail.com>
2022-02-19 20:49:20 -0600

    Created register method that encrypts password and stores the hash and salt into the user document

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,10 +1,14 @@
 const User = require("../models/User");
+const { encryptPassword } = require("../utility/passwordHelpers");
 
 module.exports = {
     register: async (req, res) => {
+        let userInfo = req.body; 
         try {
-            
-            res.status(200).json("gigity");
+            const encrypted = encryptPassword(req.body.password);
+            const newUser = await User.create({...encrypted, ...userInfo});
+            const { hash, salt, ...others } = newUser._doc;
+            res.status(200).json(others);
         } catch (err) {
             res.status(500).json(err);
         }
--- server/utility/passwordHelpers.js
+++ server/utility/passwordHelpers.js
@@ -1,6 +1,5 @@
 const crypto = require("crypto");
 
-
 module.exports.encryptPassword = (password) => {
     let salt = crypto.randomBytes(32).toString("hex");
     let hash = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString("hex");
