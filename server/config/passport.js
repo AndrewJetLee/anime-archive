358c6dc2f608dc3b8c9e0d97515b754eb137d349
AndrewJetLee <andrewjetlee777@gmail.com>
2022-02-19 20:57:00 -0600

    Edited passport config; added serializeUser and deserializeUser; integrated validatePassword helper to config

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,14 +1,38 @@
 const passport = require("passport");
 const LocalStrategy = require('passport-local').Strategy;
 const User = require("../models/User");
+const { validatePassword } = require("../utility/passwordHelpers");
 
-passport.use(new LocalStrategy(
-    function(username, password, done) {
-      User.findOne({ username: username }, function (err, user) {
-        if (err) { return done(err); }
-        if (!user) { return done(null, false); }
-        if (!user.verifyPassword(password)) { return done(null, false); }
-        return done(null, user);
-      });
+const verifyUser = async (username, password, done) => {
+    try {
+        const user = await User.findOne({username});
+        if (!user) {
+            return done(null, false);
+        }
+
+        const isValid = validatePassword(password, user.hash, user.salt);
+
+        if (isValid) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+        }
+
+    } catch (err) {
+        done(err);
     }
-  ));
+}
+
+const strategy = new LocalStrategy(verifyUser);
+
+passport.use(strategy);
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function (err, user) {
+      done(err, user);
+    });
+  });
