f96b27e50d0e0ebd89ad31ec4514e3061ec2ef58
AndrewJetLee <andrewjetlee777@gmail.com>
2022-02-20 22:07:49 -0600

    Added styled components to Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import styled from "styled-components";
+import Nav from "../components/Nav";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <Container>
+      <Wrapper>
+        <Nav />
+      </Wrapper>
+    </Container>
+  );
+};
 
-export default Login
+export default Login;
+
+const Container = styled.div`
+  display: flex;
+  width: 100vw;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 70vw;
+  flex-direction: column;
+  min-height: 100vh;
+`;
