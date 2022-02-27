d20de27e298f0ad80a78439d0e614cb2da650547
AndrewJetLee <andrewjetlee777@gmail.com>
2022-02-26 18:36:20 -0600

    Addded styled components and basic styling to footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,22 @@ import styled from "styled-components";
 const Footer = () => {
   return (
     <Container>
-      Footer
+      <Center>
+        <Top>
+          <Socials>Icons</Socials>
+        </Top>
+        <Bottom>
+          <Navigation>
+            <li><a href="">Home</a></li>
+            <li><a href="">Anime</a></li>
+            <li><a href="">Manga</a></li>
+            <li><a href="">List</a></li>
+          </Navigation>
+          <Copyright>
+            @2021 JetLee
+          </Copyright>
+        </Bottom>
+      </Center>
     </Container>
   )
 }
@@ -12,7 +27,36 @@ export default Footer
 
 const Container = styled.div`
     width: 100%;
-    height: 300px;
+    height: 250px;
     background-color: ${props => props.theme.main};
     color: white;
+    display: flex;
+    justify-content: center;
+`
+
+const Center = styled.div`
+    width: 70%;
+    background-color: green;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+`
+
+const Top = styled.div`
+    display: flex;
+    flex: 1;
+`
+
+const Bottom = styled.div`
+   flex: 3;
+`
+
+const Socials = styled.div`
+ 
+`
+const Navigation = styled.ul`
+  display: flex;
+`
+const Copyright = styled.div`
+  
 `
