4d2dbc24d9af50000e97c943ba28b971a5dc3c19
AndrewJetLee <andrewjetlee777@gmail.com>
2022-03-07 21:52:33 -0600

    Created basic layout for footer top component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,90 +1,151 @@
 import styled from "styled-components";
-import InstagramIcon from '@mui/icons-material/Instagram';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const Footer = () => {
   return (
     <Container>
       <LinksWrapper>
-        Links
-    
+        <Links>
+          <TopAnime>
+            <LinkTitle>Top Anime</LinkTitle>
+            <LinkList>
+              <LinkListItem>item</LinkListItem>
+              <LinkListItem>item</LinkListItem>
+              <LinkListItem>item</LinkListItem>
+            </LinkList>
+          </TopAnime>
+          <TopManga>
+            <LinkTitle>Top Manga</LinkTitle>
+            <LinkList>
+              <LinkListItem>item</LinkListItem>
+              <LinkListItem>item</LinkListItem>
+              <LinkListItem>item</LinkListItem>
+            </LinkList>
+          </TopManga>
+          <PopularCharacters>
+            <LinkTitle>Most Popular Characters</LinkTitle>
+            <LinkList>
+              <LinkListItem>item</LinkListItem>
+              <LinkListItem>item</LinkListItem>
+              <LinkListItem>item</LinkListItem>
+            </LinkList>
+          </PopularCharacters>
+        </Links>
       </LinksWrapper>
       <Content>
         <Top>
           <Socials>
-            <GitHubIcon className="icons github"/>
-            <LinkedInIcon className="icons linkedin"/>
-            <InstagramIcon className="icons instagram"/>
-            <TwitterIcon className="icons twitter"/>
-            <FacebookIcon className="icons facebook"/>
+            <GitHubIcon className="icons github" />
+            <LinkedInIcon className="icons linkedin" />
+            <InstagramIcon className="icons instagram" />
+            <TwitterIcon className="icons twitter" />
+            <FacebookIcon className="icons facebook" />
           </Socials>
         </Top>
         <Bottom>
           <Navigation>
-            <li><a href="/">Home</a></li>
-            <li><a href="/anime">Anime</a></li>
-            <li><a href="/manga">Manga</a></li>
-            <li><a href="/list">List</a></li>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/anime">Anime</a>
+            </li>
+            <li>
+              <a href="/manga">Manga</a>
+            </li>
+            <li>
+              <a href="/list">List</a>
+            </li>
           </Navigation>
           <ProjectsWrapper>
             <span>Personal projects:</span>
             <Projects>
-            <Project>E-commerce</Project>
-            <Project>Secure Your Vote</Project>
+              <Project>E-commerce</Project>
+              <Project>Secure Your Vote</Project>
             </Projects>
           </ProjectsWrapper>
-          <Copyright>
-            @2021 Jet Lee
-          </Copyright>
+          <Copyright>@2021 Jet Lee</Copyright>
         </Bottom>
       </Content>
     </Container>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
 
 const Container = styled.div`
-    margin-top: 50px;
-    width: 100%;
-    height: 400px;
-    background-color: ${props => props.theme.main};
-    color: white;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    overflow: hidden;
-
-`
+  margin-top: 50px;
+  width: 100%;
+  height: 400px;
+  background-color: ${(props) => props.theme.main};
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
 
 const LinksWrapper = styled.section`
-  background-color: ${props => props.theme.secondary};
+  background-color: ${(props) => props.theme.secondary};
   width: 100%;
   flex: 1;
-`
+  display: flex;
+  justify-content: center;
+  color: black;
+`;
+
 const Links = styled.div`
-  
-`
+  width: 70%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const TopAnime = styled.div`
+  flex: 1;
+  padding: 20px 0;
+  margin-right: 10px;
+`;
+const TopManga = styled(TopAnime)``;
+
+const PopularCharacters = styled(TopAnime)`
+  margin-right: 0;
+`;
+
+const LinkTitle = styled.div`
+  font-size: 1.5rem;
+  border-bottom: 1px lightgray solid;
+  padding-bottom: 10px;
+`;
+
+const LinkList = styled.ul`
+  font-size: 1.4rem;
+`;
+
+const LinkListItem = styled.li`
+  color: ${(props) => props.theme.main};
+  margin-bottom: 15px;
+`;
 
 const Content = styled.section`
-    width: 70%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    flex: 2;
-`
+  width: 70%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  flex: 2;
+`;
 
 const Top = styled.div`
-    display: flex;
-    flex: 1;
-    justify-content: center;
-    margin-top: 30px;
-`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  margin-top: 30px;
+`;
 const Socials = styled.div`
   display: flex;
   justify-content: space-around;
@@ -93,24 +154,24 @@ const Socials = styled.div`
   .icons {
     font-size: 3.5rem;
   }
-`
+`;
 
 // Bottom footer
 const Bottom = styled.div`
-   display: flex;
-   flex: 3;
-   font-size: 1.3rem;
-   flex-direction: column;
-   align-items: center;
-   justify-content: space-around;
-`
+  display: flex;
+  flex: 3;
+  font-size: 1.3rem;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+`;
 
 const Navigation = styled.ul`
   display: flex;
   margin: 0;
   li {
     margin: 0 10px;
-    :first-child{
+    :first-child {
       border-right: 1px solid;
       padding-right: 20px;
     }
@@ -118,7 +179,7 @@ const Navigation = styled.ul`
   a {
     text-decoration: none;
   }
-`
+`;
 const ProjectsWrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -130,13 +191,13 @@ const ProjectsWrapper = styled.div`
     font-size: 1.1rem;
     font-style: italic;
   }
-`
+`;
 const Projects = styled.div`
   height: 100%;
   display: flex;
   justify-content: center;
   margin-left: 40px;
-`
+`;
 
 const Project = styled.a`
   padding: 8px 19px;
@@ -144,9 +205,6 @@ const Project = styled.a`
   margin-right: 12px;
   border-radius: 4px;
   font-weight: 500;
-`
+`;
 
-const Copyright = styled.div`
-  
-`
-
+const Copyright = styled.div``;
